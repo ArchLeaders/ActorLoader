6f7e55b1c2bee698e2ef94b80f98024af7cf03dc
ArchLeaders <archparliament@gmail.com>
2023-02-08 01:25:01 -0800

    Update mod processor

--- src/ModFolder.cs
+++ src/ModFolder.cs
@@ -1,4 +1,5 @@
-﻿using Nintendo.Byml;
+﻿using Byml.Security.Cryptography;
+using Nintendo.Byml;
 using Yaz0Library;
 
 namespace ActorLoader;
@@ -14,9 +15,10 @@ public class ModFolder
 
 	private readonly string _srcActorsPath = Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location) ?? string.Empty, "Data", "Actors");
 	private readonly BymlFile _srcActorInfo = Resource.GetSrcActorInfo();
-	private readonly string[] _srcActors;
+	private readonly List<string> _srcActors;
 
 	private readonly string[] _ignoredActors = Resource.GetIgnoredList();
+	private readonly uint[] _vanillaActors = Resource.GetVanillaActorsList();
 
     public ModFolder(string path, bool auto)
 	{
@@ -29,19 +31,23 @@ public class ModFolder
         _actorInfo = Resource.GetModActorInfo(Path.Combine(_path, "content", "Actor", "ActorInfo.product.sbyml"));
         _actors = Directory.EnumerateFiles(_actorsPath).Select(x => Path.GetFileNameWithoutExtension(x)!).ToList();
 		
-		_srcActors = _srcActorInfo.RootNode.Hash.Keys.ToArray();
+		_srcActors = _srcActorInfo.RootNode.Hash.Keys.ToList();
     }
 
-	public Task Compute()
+	public async Task Compute()
 	{
-		return IterateFolders(_path);
+		await IterateFolders(_path);
+
+		byte[] data = _actorInfo.ToBinary();
+		data = Yaz0.Compress(data.AsSpan(), out Yaz0SafeHandle _).ToArray();
+		File.WriteAllBytes(Path.Combine(_path, "content", "Actor", "ActorInfo.product.sbyml"), data);
     }
 
 	private async Task IterateFolders(string path)
 	{
 		await IterateFiles(path);
 		await Parallel.ForEachAsync(Directory.EnumerateDirectories(path), async (folder, cancellationToken) => {
-			await IterateFolders(path);
+			await IterateFolders(folder);
 		});
 	}
 
@@ -54,39 +60,54 @@ public class ModFolder
 
 	private async Task ProcessMubin(string path)
 	{
+		Console.WriteLine($"[{DateTime.Now:u}] [Processing] | '{Path.GetFileNameWithoutExtension(path)}'");
+
 		byte[] mubinData = Yaz0.Decompress(path).ToArray();
 		BymlNode mubin = new BymlFile(mubinData).RootNode;
 
-		foreach (var obj in mubin.Hash["Objs"].Array.Select(x => x.Hash)) {
-			string name = obj["UintConfigName"].String;
+		BymlNode? diff = null;
 
-			// Ignore the actor if it's flaged
-			// to be ignored or already exists
-			if (_ignoredActors.Contains(name) || _actors.Contains(name)) {
+        if (_auto) {
+            string map = Path.GetFileName(Path.GetDirectoryName(Path.GetDirectoryName(path)!)!);
+            string unit = Path.GetFileName(Path.GetDirectoryName(path)!);
+            string type = Path.GetFileName(path).Split('_')[1];
+			diff = Resource.GetVanillaMapEntry(unit, map, type).RootNode;
+        }
+
+		foreach (var obj in mubin.Hash["Objs"].Array.Select(x => x.Hash)) {
+			string name = obj["UnitConfigName"].String;
+
+			// Ignore the actor if it's flaged to
+			// be ignored, already exists or is a
+			// vanilla actor.
+			if (_vanillaActors.Contains(Crc32.Compute(name)) || _ignoredActors.Contains(name) || _actors.Contains(name)) {
 				continue;
 			}
 
 			if (_auto && _srcActors.Contains(name + 'C')) {
 				name += 'C';
 				obj["UintConfigName"] = new(name);
+
+				// Check if it's a vanilla entry
+				if (diff!.Hash["Objs"].Array.Select(x => x.Hash["HashID"].UInt).Contains(obj["HashID"].UInt)) {
+					continue;
+				}
 			}
 
 			if (_srcActors.Contains(name)) {
-				Console.WriteLine($"[{DateTime.Now:u}] [Processing] | '{name}'");
-
-				string cActor = Path.Combine(_srcActorsPath, name, ".sbactorpack");
+				string cActor = Path.Combine(_srcActorsPath, $"{name}.sbactorpack");
 				if (!File.Exists(cActor)) {
 					await Resource.DownloadCActor(cActor, name);
 				}
 
-                File.Copy(Path.Combine(_srcActorsPath, name, ".sbactorpack"), Path.Combine(_actorsPath, name, ".sbactorpack"));
+				File.Copy(Path.Combine(_srcActorsPath, $"{name}.sbactorpack"), Path.Combine(_actorsPath, $"{name}.sbactorpack"), true);
 				_actorInfo.RootNode.Hash["Actors"].Array.Add(
 					_srcActorInfo.RootNode.Hash[name]
 				);
 
-				Console.WriteLine($"  -> [{DateTime.Now:u}] [Updated] | '{name}'");
+				_srcActors.Add(name);
+                Console.WriteLine($"  -> [{DateTime.Now:u}] [Updated] -> '{name}'");
 			}
-
 		}
 	}
 
