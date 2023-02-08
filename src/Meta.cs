68eaa3db5691c9a23f338fd6254103e536a9f03f
ArchLeaders <archparliament@gmail.com>
2023-02-08 02:00:25 -0800

    Create help command

---
+++ src/Meta.cs
@@ -0,0 +1,26 @@
+﻿namespace ActorLoader;
+
+public static class Meta
+{
+    public static string Version { get; } = typeof(Program).Assembly.GetName().Version!.ToString(3);
+    public static string Help { get; } = $"""
+         Actor Loader, v{Version}
+         
+         (c) ArchLeaders 2023, under the AGPL-3.0 license
+         - - - - - - - - - - - - - - - - - - - - - - - - -
+         
+         
+         ActorLoader.exe <path> [-a|--auto] [-p|--parallel]
+         
+         
+         path: The path to the root mod folder (e.g. the
+             folder containing 'content')
+         
+         auto: Automatically append C to valid actors that
+             don't exist in the vanilla game files
+         
+         parallel: Iterate the mod directories in parallel (asynchronously)
+                 Warning: this can cause issues if some actors
+                 weren't previously downloaded
+        """;
+}
--- src/Program.cs
+++ src/Program.cs
@@ -8,13 +8,17 @@ bool parallel = false;
 if (args.Length > 0) {
     path = args[0];
 
-    foreach (var arg in args[1..].Where(x => x.StartsWith('-'))) {
+    foreach (var arg in args.Where(x => x.StartsWith('-'))) {
         string argName = arg.ToLower()[1..];
         if (argName == nameof(auto) || argName[0] == nameof(auto)[0]) {
             auto = true;
         }
         else if (argName == nameof(parallel) || argName[0] == nameof(parallel)[0]) {
             parallel = true;
+        }
+        else if (argName == "help" || argName[0] == 'h') {
+            Console.WriteLine(Meta.Help);
+            Environment.Exit(0);
         }
     }
 }
